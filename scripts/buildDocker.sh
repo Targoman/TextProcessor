d1787987c9ea2fa5fe70d01d9bfbf5bd4dbb65c0
S. Mohammad M. Ziabary <softnhard.es@gmail.com>
2018-02-18 01:07:22 +0330

    Update buildDocker.sh

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -3,8 +3,8 @@
 pushd $(dirname $0) >/dev/null
 cd ..
 
-docker tag targoman/textprocessor_server:latest targoman/textprocessor_server:$(date +"%y%m%d_%H%M%S")
-docker rmi targoman/textprocessor_server:latest
-docker build -t targoman/textprocessor_server:latest -f scripts/Dockerfile .
+docker tag targoman/e4mt_server:latest targoman/e4mt_server:$(date +"%y%m%d_%H%M%S")
+docker rmi targoman/e4mt_server:latest
+docker build -t targoman/e4mt_server:latest -f scripts/Dockerfile .
 
 popd > /dev/null
